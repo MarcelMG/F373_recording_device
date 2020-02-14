a999ceb164dac029076097643451c888198553fc
MarcelMG <28888720+MarcelMG@users.noreply.github.com>
2020-02-14 11:09:02 +0100

    read gain configuration setting from file and convert value to Ampere correctly

--- software/bin2hdf_batch_convert.py
+++ software/bin2hdf_batch_convert.py
@@ -29,8 +29,28 @@ file_list = [x for x in os.listdir(working_directory) if ".bin" in x]
 # iterate over all *.bin files and convert them to HDF5 pandas dataframe format
 for i in range(len(file_list)):
     filepath = working_directory + "/" + file_list[i]
+    # read gain configuration setting from file
+    my_file = open(filepath, "rb")
+    # read first 3 bytes of file and decode bytes to string
+    cfg = my_file.read(3).decode("ascii")
+    my_file.close()
+    # define gain settings and corresponding gain factors as dictionary
+    gain_cfg_dic = {0:10E7,
+                    1:10E6,
+                    2:10E5,
+                    3:10E4,
+                    4:10E3,
+                    5:10E2}
+    gain = [0, 0, 0]
+    for j in range(3):
+        gain_setting = int(cfg[j])
+        if gain_setting in gain_cfg_dic:
+            gain[j] = gain_cfg_dic[gain_setting]
+        else:
+            print("error reading gain setting from file.")
+            quit()
     # read data from file
-    data = np.fromfile(filepath, np.int16) # read binary data in signed 16bit integer format
+    data = np.fromfile(filepath, np.int16, offset=3) # read binary data in signed 16bit integer format, skip first 3 bytes which contain gain setting
     data = data.astype(np.int32) # convert array to int32 (else the added offset would overflow the 16bit integer)
     data += 32768 # add offset
     data = data.astype(np.float) # convert to floating point value
@@ -60,6 +80,10 @@ for i in range(len(file_list)):
         channel1_data = np.concatenate((channel1_data, data[ k*500 : (k+1)*500 ]))
         channel2_data = np.concatenate((channel2_data, data[ (k+1)*500 : (k+2)*500 ]))
         channel3_data = np.concatenate((channel3_data, data[ (k+2)*500 : (k+3)*500 ]))
+    # convert ADC voltage to photo-current(~= illuminance) by dividing by the amplifier's gain factor
+    channel1_data /= gain[0]
+    channel2_data /= gain[1]
+    channel3_data /= gain[2]
     # create a dictionary dataset with the data of all 3 channels
     dataset = {'channel1' : channel1_data, 'channel2' : channel2_data, 'channel3' : channel3_data }
     # create a pandas dataframe from the dictionary
